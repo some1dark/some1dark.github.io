3ebd11548bd2d70daa9c0bd7e8823cfea3bf3b98
some1dark <stephen.morris@outlook.de>
2016-06-25 10:05:11 +0200

    Updates

--- responcive/js/scripts.js
+++ responcive/js/scripts.js
@@ -1,5 +1,5 @@
 // Main Navigation
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function myNavFunction() {
 	document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
 }
