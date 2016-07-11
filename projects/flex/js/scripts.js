c5149353c1e87896eed826f59b5523b00bea5fe9
some1dark <stephen.morris@outlook.de>
2016-07-11 14:31:49 +0200

    Bootstraped
    Shisha Bong Club Hannover is getting Bootstraped

---
+++ projects/flex/js/scripts.js
@@ -0,0 +1,4 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
