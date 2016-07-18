b161a59ed7a7784f090be3268b2643db5e404b19
some1dark <stephen.morris@outlook.de>
2016-07-18 15:06:29 +0200

    Popup

--- projects/bootstrap/js/offcanvas.js
+++ projects/bootstrap/js/offcanvas.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
+    $('#myCarousel').carousel({
+        interval: 3000
+    })
+    $('.fdi-Carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
 });
+
+$('#overlay').modal('show');
