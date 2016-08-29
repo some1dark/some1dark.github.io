4089a848065e3a9d94c4e6b871f8b1e342120363
some1dark <stephen.morris@outlook.de>
2016-08-29 12:44:05 +0200

    .

---
+++ projects/bootstrap/js/cookie-18-over.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //Referances 
+    //jQuery Cookie : https://github.com/carhartl/jquery-cookie
+    //Modal : http://getbootstrap.com/javascript/#modals
+    var Age18_cookie = $.cookie($('.modal-check').attr('name'));
+    if (my_cookie && my_cookie == "true") {
+        $(this).prop('checked', my_cookie);
+        console.log('checked checkbox');
+    }
+    else{
+        $('#myModal').modal('show');
+        console.log('uncheck checkbox');
+    }
+
+    $(".modal-check").change(function() {
+        $.cookie($(this).attr("name"), $(this).prop('checked'), {
+            path: '/',
+            expires: 1
+        });
+    });
+});
+   
+  
