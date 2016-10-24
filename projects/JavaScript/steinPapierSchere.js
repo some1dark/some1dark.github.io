efab069844273a2ab8f8c0bc6a347308f2a226e8
Stephen James Morris <some1dark@users.noreply.github.com>
2016-10-24 12:49:28 +0200

    Update steinPapierSchere.js
    Neue art der function() Schreibweise.

--- projects/JavaScript/steinPapierSchere.js
+++ projects/JavaScript/steinPapierSchere.js
@@ -7,28 +7,33 @@
 "use strict"
 
 let choices = ["stein", "papier", "schere"];                    // die möglichen Auswahlen die getroffen werden dürfen
-let userAuswahl = "";                                           // die Variable um die vom User eigabe zu speichern
-let computerAuswahl = Math.random();                            // 0.00 bis 1.00.
 
-if (computerAuswahl < 0.34) {                                   // ab 0.00 bis 0.33 wird Stein gewählt
-    computerAuswahl = "stein";
-} else if(computerAuswahl <= 0.67) {                            // ab 0.34 bis 0.66 wird Papier gewählt
-    computerAuswahl = "papier";
-} else {                                                        // ab 0.67 bis 1.00 wird Schere gewählt
-    computerAuswahl = "schere";
-} 
+let auswahl = () => {
+    let auswahl = Math.floor(Math.random() * 3 +1);             // mit Math.random() * 3 +1 wird die Zahl von 0.01 - 0.99 mit 3 multipliziert & darauf wird 1 addiert. Math.floor() nimmt alle Dezimalstellen weg um eine gerade Zahl zu erhalten
 
-let userEingabe = function() {
+    switch(auswahl) {
+        case 1: auswahl = "stein";                              // bei 1 wird "Stein" gewählt
+            break;
+        case 2: auswahl = "papier";                             // bei 2 wird "Papier" gewählt
+            break;
+        case 3:  auswahl = "schere";                            // bei 3 wird "Schere" gewählt
+            break;
+    }
+    return auswahl;                                             // die Ausgabe der Auswahl
+}
+
+let eingabe = () => {
+    let userAuswahl = "";
     while (!choices.includes(userAuswahl)) {                    // solange der boolean "true" bleibt die Funktion in der Schleife
         if(userAuswahl.length !== 0) {                          // wenn der User eine falsche Eingabe gemacht hat
             alert("Sie haben eine falsche Auswahl getroffen.\nBitte versuchen sie es erneut!");
         }                                                       // hier werden die vom User eingegebe Eingabe klein gestellt
         userAuswahl = prompt("Wähle: Stein, Papier oder Schere").toLowerCase();
     }
+    return userAuswahl;
 }
-userEingabe();
 
-let vergleiche = function(Auswahl1, Auswahl2) {                 // vergleiche die Auswahlen
+let vergleiche = (Auswahl1, Auswahl2) =>  {                     // vergleiche die Auswahlen in der function()
     if(Auswahl1 === Auswahl2) {                                 // Auswahl1 ist der User & Auswahl2 ist der Computer
         return "Es ist uentschieden!"                           // wenn beide die selbe Wahl haben ist es unentschieden
 
@@ -58,6 +63,9 @@ let vergleiche = function(Auswahl1, Auswahl2) {                 // vergleiche di
     }
 };
 
+let computerAuswahl = auswahl();                                // ausgabe der eingabe Funktion (Computer)
+let userEingabe = eingabe();                                    // ausgabe der eingabe Funktion (User)
+
 console.log("Computer: " + computerAuswahl);                    // Anzeige des Math.random() die der Computer zugeteilt bekommen hat
-console.log("User: " + userAuswahl);                            // Anzeige des vom User eingegebene Auswahl
-console.log(vergleiche(userAuswahl, computerAuswahl));          // hier wird ausgegeben wer gewonnen hat
+console.log("User: " + userEingabe);                            // Anzeige des vom User eingegebene Auswahl
+console.log(vergleiche(userEingabe, computerAuswahl));          // hier wird ausgegeben wer gewonnen hat
